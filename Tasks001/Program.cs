5d9744aa207afc9fa3a3bb8048796f69dc6ed1ee
KOST <KSK>
2024-02-10 17:56:47 +0200

    Добавил решение задачи №1

---
+++ Tasks001/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 1: 
+// Напишите программу, которая принимает на вход число и проверяет, 
+// кратно ли оно одновременно 7 и 23.
+
+System.Console.WriteLine("Введите число" + " ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if(N % 7 == 0 && N % 23 ==0)
+{
+    System.Console.Write($"Данное число {N} одновременно кратно 7 и 23");
+}
+ else if(N % 7 == 0 )
+ {
+    System.Console.Write("Число кратно только 7");
+ }
+ else if(N % 23 == 0)
+ {
+    System.Console.Write("Число кратно только 23");
+ }
+else
+{
+    System.Console.Write("Чило не кратно одновременно 7 и 23");
+}
