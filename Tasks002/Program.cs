2a5daf79c95759b3675304c4bea1b728586a2946
KOST <KSK>
2024-02-13 22:38:53 +0200

    add Task002: Решение задачи №2

---
+++ Tasks002/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 2: Напишите программу, которая принимает
+// на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠
+// 0 и выдаёт номер координатной четверти плоскости,
+// в которой находится эта точка.
+
+System.Console.Write("Введите координаты точки Х: ");
+int X = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Введите координаты точки Y: ");
+int Y = Convert.ToInt32(Console.ReadLine());
+
+if(X > 0 && Y > 0)
+{
+    System.Console.WriteLine($"Точка находится в четверти № 1 ");
+}
+else if(X < 0 && Y > 0)
+{
+    System.Console.WriteLine($"Точка находится в четверти № 2 ");
+}
+else if(X < 0 && Y < 0)
+{
+    System.Console.WriteLine($"Точка находится в четверти № 3 ");
+}
+else if(X > 0 && Y < 0)
+{
+    System.Console.WriteLine($"Точка находится в четверти № 4 ");
+}
+else 
+{
+    System.Console.WriteLine($"Точка находится на оси координат ");
+}
+
+
+
