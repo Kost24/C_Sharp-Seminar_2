2f4a61287a3f966212ad198f3d6631340d5d9f9b
KOST <KSK>
2024-02-14 22:33:49 +0200

    add Task004: Решение задачи №4

---
+++ Tasks004/Program.cs
@@ -0,0 +1,13 @@
+﻿
+// Задача 4: Напишите программу, которая на вход 
+// принимает натуральное число N, а на выходе 
+// показывает его цифры через запятую.
+
+
+System.Console.Write("Введите число : ");
+long n = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write(n % 10 + ", ");
+while((n /= 10) !=0)
+System.Console.Write(n % 10 + ", ");
+
